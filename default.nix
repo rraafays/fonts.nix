ded341f9674808d2051729b5e2398a59fe636e7a
raf <rraf@tuta.io>
2024-09-27 17:49:07 +0100

    chore: set default font

--- default.nix
+++ default.nix
@@ -1,11 +1,20 @@
 { pkgs, ... }:
 
 {
-  fonts.packages = with pkgs; [
-    (iosevka-bin.override { variant = "SGr-IosevkaTermCurly"; })
-    (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
-    sarasa-gothic
-    sarabun-font
-    noto-fonts-emoji
-  ];
+  fonts = {
+    packages = with pkgs; [
+      (iosevka-bin.override { variant = "SGr-IosevkaTermCurly"; })
+      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+      sarasa-gothic
+      sarabun-font
+      noto-fonts-emoji
+    ];
+    fontconfig = {
+      defaultFonts = {
+        serif = [ "Iosevka Term Cruly" ];
+        sansSerif = [ "Iosevka Term Cruly" ];
+        monospace = [ "Iosevka Term Cruly" ];
+      };
+    };
+  };
 }
